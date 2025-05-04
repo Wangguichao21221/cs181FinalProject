9769b9a03f924457f17181754de387d9b80f95d7
Wangguichao21221 <wanggch2023@shanghaitech.edu.cn>
2025-05-04 09:27:17 +0800

    initial commit of project 4

---
+++ tracking/submit.py
@@ -0,0 +1,32 @@
+# submit.py
+# -------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to ShanghaiTech University, including a link 
+# to https://i-techx.github.io/iTechX/courses?course_code=CS181
+
+
+import tarfile
+import zipfile
+from projectParams import STUDENT_CODE_DEFAULT
+
+# def tar(fname, files):
+#     with tarfile.open(fname, "w") as tar:
+#         for f in files:
+#             tar.add(f)
+
+# tar("tracking.tar", STUDENT_CODE_DEFAULT.split(","))
+# print("""Successfully generated tar file. Please submit the generated tar file to
+# Autolab to receive credit.""")
+
+
+def zip(fname, files):
+    with zipfile.ZipFile(fname, 'w') as zipf:
+        for f in files:
+            zipf.write(f, arcname=f.split('/')[-1])
+
+zip("tracking.zip", STUDENT_CODE_DEFAULT.split(","))
+print("""Successfully generated zip file. Please submit the generated zip file to
+Gradescope to receive credit.
+""")
